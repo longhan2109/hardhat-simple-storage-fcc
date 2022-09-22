14c9d24cbf82a9a8291f54db7e91e7eb68ddd987
longhan2109 <namlong9012@gmail.com>
2022-09-23 00:30:01 +0700

    verify and interact to deployed smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,39 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 const main = async () => {
-  const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
 
-  console.log('deploying ...')
+  console.log("deploying ...");
 
   const simpleStorage = await SimpleStorageFactory.deploy();
-  await simpleStorage.deployed()
-  console.log(`Deployed contract address: ${simpleStorage.address}`)
+  await simpleStorage.deployed();
+  console.log(`Deployed contract address: ${simpleStorage.address}`);
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    // on Goerli and has Etherscan api key
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current value: ${currentValue}`);
+
+  // update current value
+  const txResponse = await simpleStorage.store(7);
+  await txResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated value: ${updatedValue}`);
 };
 
-const verify = async (contractAddress, params) => {
-  console.log('Verifying...')
-  
-}
+const verify = async (contractAddress, args) => {
+  console.log("Verifying...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 main();
